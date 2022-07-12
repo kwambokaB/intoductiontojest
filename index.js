6975c11ed96018bf3ede5262a711c4c52f5c8580
kwambokaB <paula.paulatek@gmail.com>
2022-07-12 16:45:05 +0300

    updated test

--- index.js
+++ index.js
@@ -1,4 +1,11 @@
 module.exports = (num) => {
-    if (num % 15 === 0) return 'fizzbuzz';
+    if (num % 15 === 0){
+        return 'fizzbuzz';
+    }else if(num % 5 === 0){
+        return 'buzz';
+    }else if(num % 3 === 0){
+        return 'fizz';
+    }else{
     return `${num}`
+    }
   };
--- index.test.js
+++ index.test.js
@@ -8,5 +8,13 @@ describe('fizzbuzz()', () => {
     it('returns the given number for multiples of neither 3 nor 5', () => {
         expect(fizzbuzz(1)).toBe('1');
         expect(fizzbuzz(22)).toBe('22');
-      });
+    });
+    it('returns fizz for multiples of 3', () => {
+        expect(fizzbuzz(6)).toBe('fizz');
+        expect(fizzbuzz(21)).toBe('fizz');
+    });
+    it('returns buzz for multiples of 5', () => {
+        expect(fizzbuzz(20)).toBe('buzz');
+        expect(fizzbuzz(35)).toBe('buzz');
+    });
 })
